62bac59bb37b138f2376ca0c62de7821cba80702
lizhenbin <lizhenbin@oneplus.cn>
2015-07-10 14:46:29 +0800

    增加索引循环

--- src/main/java/com/oneplus/mybatis/generat/config/DefaultGeneratorConfigurer.java
+++ src/main/java/com/oneplus/mybatis/generat/config/DefaultGeneratorConfigurer.java
@@ -51,6 +51,7 @@ public class DefaultGeneratorConfigurer implements GeneratorConfigurer {
         initDomain();
         initLayers();
         initLocation();
+        initJavaSrc();
     }
 
     protected void initPackage() {
